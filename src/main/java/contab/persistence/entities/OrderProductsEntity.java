993e49b7d744e5cf7e355c4384ba234d15062323
Joao Vitor <joao.vpereira@a.ficr.edu.br>
2025-05-05 20:54:10 -0300

    minha implementação do orders

---
+++ src/main/java/contab/persistence/entities/OrderProductsEntity.java
@@ -0,0 +1,111 @@
+package contab.persistence.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_order")
+public class OrderProductsEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "order_product_id", nullable = false)
+    private Long orderProductId;
+
+    @Column(name = "order_id", nullable = false)
+    private Long orderId;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String barcode;
+
+    @Column(nullable = false)
+    private Long amount;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer subTotal;
+
+    @Column(name = "is_deleted", nullable = false)
+    private boolean isDeleted;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    public Long getOrderProductId() {
+        return orderProductId;
+    }
+
+    public void setOrderProductId(Long orderProductId) {
+        this.orderProductId = orderProductId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Long orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBarcode() {
+        return barcode;
+    }
+
+    public void setBarcode(String barcode) {
+        this.barcode = barcode;
+    }
+
+    public Long getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Long amount) {
+        this.amount = amount;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getSubTotal() {
+        return subTotal;
+    }
+
+    public void setSubTotal(Integer subTotal) {
+        this.subTotal = subTotal;
+    }
+
+    public boolean isDeleted() {
+        return isDeleted;
+    }
+
+    public void setDeleted(boolean deleted) {
+        isDeleted = deleted;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+}
---
+++ src/main/java/contab/persistence/entities/OrdersEntity.java
@@ -0,0 +1,122 @@
+package contab.persistence.entities;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_orders", schema = "public")
+public class OrdersEntity {
+
+@Id
+@GeneratedValue(strategy = GenerationType.AUTO)
+@Column(name = "order_id", nullable = false)
+private Integer orderId;
+
+@Column(nullable = false)
+private String document;
+
+@Column(nullable = false)
+private String company;
+
+@Column(nullable = false)
+private Integer total;
+
+@Column(name = "is_created", nullable = false)
+private boolean isCreated;
+
+@Column(name = "is_finished", nullable = false)
+private boolean isFinished;
+
+@Column(name = "is_canceled", nullable = false)
+private boolean isCanceled;
+
+@Column(name = "is_invalid", nullable = false)
+private boolean isInvalid;
+
+@Column(name = "created_at", nullable = false)
+private LocalDateTime createdAt;
+
+@Column(name = "status_updated_at", nullable = false)
+private LocalDateTime statusUpdatedAt;
+
+    public Integer getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(Integer orderId) {
+        this.orderId = orderId;
+    }
+
+    public String getDocument() {
+        return document;
+    }
+
+    public void setDocument(String document) {
+        this.document = document;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public Integer getTotal() {
+        return total;
+    }
+
+    public void setTotal(Integer total) {
+        this.total = total;
+    }
+
+    public boolean isCreated() {
+        return isCreated;
+    }
+
+    public void setCreated(boolean created) {
+        isCreated = created;
+    }
+
+    public boolean isFinished() {
+        return isFinished;
+    }
+
+    public void setFinished(boolean finished) {
+        isFinished = finished;
+    }
+
+    public boolean isCanceled() {
+        return isCanceled;
+    }
+
+    public void setCanceled(boolean canceled) {
+        isCanceled = canceled;
+    }
+
+    public boolean isInvalid() {
+        return isInvalid;
+    }
+
+    public void setInvalid(boolean invalid) {
+        isInvalid = invalid;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getStatusUpdatedAt() {
+        return statusUpdatedAt;
+    }
+
+    public void setStatusUpdatedAt(LocalDateTime statusUpdatedAt) {
+        this.statusUpdatedAt = statusUpdatedAt;
+    }
+}
