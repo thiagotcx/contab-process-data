a95ae485ac24db614e3aa07def2d9b59e28a4738
Thiago Soares <thiagosb.moura@gmail.com>
2025-05-14 23:19:54 -0300

    alterar ProductService para usar lombok

--- src/main/java/contab/services/impl/ProductService.java
+++ src/main/java/contab/services/impl/ProductService.java
@@ -3,18 +3,16 @@ package contab.services.impl;
 import contab.dto.ProductDTO;
 import contab.persistence.repositories.ProductRepository;
 import contab.services.IProductService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@RequiredArgsConstructor
 @Service
 public class ProductService implements IProductService {
 
     private final ProductRepository productRepository;
-
-    public ProductService(ProductRepository productRepository) {
-        this.productRepository = productRepository;
-    }
 
     @Override
     public List<ProductDTO> getActiveProducts() {
